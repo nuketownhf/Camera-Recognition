603a334ad40928d06ad6068533a393ffac1d1f26
nuketownhf <nuketownhf@gmail.com>
2017-12-07 15:09:24 +0100

    Augenerkennung

--- ipcam.py
+++ ipcam.py
@@ -2,30 +2,35 @@ import cv2
 import numpy as np
 
 
-host = '192.168.137.72:8080'
-benutzer = input("Account \n" )
-passwort = input("passwort \n" )
+host = '192.168.0.59:8080'
+benutzer = 'admin'
+passwort = '1234'
 
 hoststr = 'http:{0}:{1}@{2}/video'.format(benutzer, passwort, host)
 print('Streame ' + hoststr)
 
 aufzeichnung = cv2.VideoCapture(hoststr)
-
+augenerkennung = cv2.CascadeClassifier('objekte/haarcascade_eye.xml')
 gesichtserkennung = cv2.CascadeClassifier('objekte/haarcascade_frontalface_default.xml')
 while True:
 
-	ret, img = aufzeichnung.read()
-	grau = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	gesicht = gesichtserkennung.detectMultiScale(grau, 1.3, 5)
+    ret, frame = aufzeichnung.read()
+    grau = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gesicht = gesichtserkennung.detectMultiScale(grau, 1.3, 5)
 
+    for (x,y,w,h) in gesicht:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+        roi_grau = grau[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        augen = augenerkennung.detectMultiScale(roi_grau)
+        for (ax, ay, aw, ah) in augen:
+                        cv2.rectangle(roi_color, (ax, ay), (ax+aw, ay+ah), (0,255,0), 2)
+                
 
-	for (x,y,w,h) in gesicht:
-		img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),2)
+    cv2.imshow('Gesichtserkennung', frame)
 
-	cv2.imshow('Gesichtserkennung', img)
-
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 print('Verlasse IP-Kamera')
 aufzeichnung.release()
